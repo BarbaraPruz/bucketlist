185bc09f8d5ad2e77068f902c039b5c4d09378a0
BarbaraPruz <barbara.prusiewicz@gmail.com>
2018-07-09 17:51:10 -0700

    Add Login handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,5 +13,31 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  post '/login' do
+    user = User.find_by(:email => params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/user_home"
+    else
+      redirect "/failure"
+    end
+  end
+
+  get '/user_home' do
+    redirect "/login" if !logged_in?
+    @user = current_user
+    @buckets = Bucket.all.find_all { |bucket| bucket.user_id == current_user.id}
+    erb :"/user_home"
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
 
 end
