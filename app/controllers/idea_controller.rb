6fd5595516ae32b095bfa95ef49be720750012d0
BarbaraPruz <barbara.prusiewicz@gmail.com>
2018-07-13 15:40:39 -0700

    Sort contents for Browse All Ideas

--- app/controllers/idea_controller.rb
+++ app/controllers/idea_controller.rb
@@ -5,8 +5,18 @@ class IdeaController < ApplicationController
 
     get '/ideas' do
         redirect "/login" if !logged_in?
-        @ideas = Idea.all
+        @ideas = Idea.all.sort { |a,b| a.name <=> b.name }
         erb :"/ideas/show"
     end  
 
+    get '/ideas/search' do
+        redirect "/login" if !logged_in?
+        @categories = Category.all
+        erb :"/ideas/search"
+    end  
+    post '/ideas/search' do
+        redirect "/login" if !logged_in?
+# params => {"category_ids"=>["1", "3"]}
+        redirect :"/ideas"  # temporary
+    end  
 end
