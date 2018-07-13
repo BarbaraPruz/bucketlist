ed4dd71de082239ac363e78b91cfeba5ee864fe2
BarbaraPruz <barbara.prusiewicz@gmail.com>
2018-07-13 16:33:15 -0700

    Correct Idea model (categories not category).  Add placeholder code for search ideas

--- app/controllers/idea_controller.rb
+++ app/controllers/idea_controller.rb
@@ -16,7 +16,10 @@ class IdeaController < ApplicationController
     end  
     post '/ideas/search' do
         redirect "/login" if !logged_in?
-# params => {"category_ids"=>["1", "3"]}
+        # params => {"category_ids"=>["1", "3"]}
+        # want to find all ideas where idea.categories contains one of the values in
+        # params[:category_ids]
+        @ideas = Idea.where(category_id: [1,3,5])
         redirect :"/ideas"  # temporary
     end  
 end
--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,4 +1,4 @@
 class Idea <ActiveRecord::Base
     has_many :category_ideas
-    has_many :categorys, through: :category_ideas
+    has_many :categories, through: :category_ideas
 end
