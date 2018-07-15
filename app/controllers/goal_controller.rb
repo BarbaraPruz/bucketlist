09dc4b82a739dd171a33f6124844fda784ca58ef
BarbaraPruz <barbara.prusiewicz@gmail.com>
2018-07-14 17:58:40 -0700

    Simplify Goal Routes

--- app/controllers/goal_controller.rb
+++ app/controllers/goal_controller.rb
@@ -20,33 +20,29 @@ class GoalController < ApplicationController
         redirect "user_home"
     end
     
-    get '/goals/:goal_id/:bucket_id/edit' do
+    get '/goals/:goal_id/edit' do
         redirect '/' if !logged_in?
-        @bucket = Bucket.find(params[:bucket_id])
-        @goal = Goal.find(params[:goal_id])
+        @goal = Goal.find(params[:goal_id])        
+        @bucket = Bucket.find(@goal.bucket_id)
         redirect '/failure' if @bucket.user_id !=current_user.id
-        redirect '/failure' if @goal.bucket_id != @bucket.id
         erb :"/goals/edit_goal"
     end
-    # QUESTION : similar to above patch route doesn't match the Get route
     patch '/goals/:goal_id' do
         redirect '/failure' if !logged_in?
-        redirect "/goals/#{params[:goal_id]}/#{params[:bucket_id]}/edit" if params[:goal][:title].length == 0
-        bucket = Bucket.find(params[:bucket_id])
+        redirect "/goals/#{params[:goal_id]}/edit" if params[:goal][:title].length == 0
         goal = Goal.find(params[:goal_id])
+        bucket = Bucket.find(goal.bucket_id)
         redirect 'failure' if bucket.user_id != current_user.id
-        redirect 'failure' if goal.bucket_id != bucket.id
         goal.update(params[:goal])
         goal.save
         redirect '/user_home'
     end
 
-    delete '/goals/:goal_id/:bucket_id/delete' do
+    delete '/goals/:goal_id/delete' do
         redirect '/failure' if !logged_in?
-        bucket = Bucket.find(params[:bucket_id])
-        goal = Goal.find(params[:goal_id])
+        goal = Goal.find(params[:goal_id])        
+        bucket = Bucket.find(goal.bucket_id)
         redirect 'failure' if bucket.user_id != current_user.id
-        redirect 'failure' if goal.bucket_id != bucket.id
         Goal.delete(params[:goal_id])
         redirect '/user_home'
     end   
